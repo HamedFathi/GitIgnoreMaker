377bedec0c9b0172b85cbbb26b2d90e320a65657
Hamed Fathi <noeffect@outlook.com>
2019-01-12 19:11:02 +0330

    Init

---
+++ GitIgnoreMaker/GitIgnore.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace GitIgnoreMaker
+{
+    public static class GitIgnore
+    {
+        public static List<string> GetList()
+        {
+            var client = new WebClient();
+            var text = client.DownloadString("https://www.gitignore.io/api/list");
+            var list = text.Split(',').Distinct().ToList();
+            list.Sort();
+            return list;
+        }
+        public static string GetTemplate(params string[] titles)
+        {
+            if (titles == null || titles.Length == 0)
+            {
+                return null;
+            }
+            var client = new WebClient();
+            var parameters = titles.Where(x => !string.IsNullOrEmpty(x)).Aggregate((x, y) => x + "," + y);
+            var template = client.DownloadString("https://www.gitignore.io/api/" + parameters);
+            return template;
+        }
+    }
+}
---
+++ GitIgnoreMakerSample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using GitIgnoreMaker;
+
+namespace GitIgnoreMakerSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gitIgnoreList = GitIgnore.GetList();
+
+            var gitIgnoreTemplate = GitIgnore.GetTemplate("visualstudio", "visualstudiocode", "rider");
+        }
+    }
+}
